55feff9a1d8812d440b7f6988c802b8e4e37aa9e
Flash <flash.jaq@gmail.com>
2019-07-12 20:58:10 +0200

    Added usermodel and validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../models/User");
 
 // @route   POST api/users
 // @desc    Register a users
 // @access  PUBLIC
-router.post("/", (req, res) => {
-  res.send("Registered a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter a name")
+      .not()
+      .isEmpty(),
+    check("email", "Please enter a valid email address").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 7 or more characters"
+    ).isLength({
+      min: 7
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+    res.send("passed");
+  }
+);
 
 module.exports = router;
