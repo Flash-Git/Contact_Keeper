bc408a8860fc09006943d6d1ef397c61df4d31de
Flash <flash.jaq@gmail.com>
2019-07-13 08:02:46 +0200

    Added getting users contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const config = require("config");
+
+const auth = require("../middleware/auth");
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 // @route   GET api/contacts
 // @desc    Get all user's contacts
 // @access  PRIVATE
-router.get("/", (req, res) => {
-  res.send("Get all contacts");
+router.get("/", auth, async (req, res) => {
+  try {
+    //Get contacts by most recent
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   POST api/contacts
