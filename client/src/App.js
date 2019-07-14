b47b2c475b7a41df152e01978af3f8584c90c796
Flash <flash.jaq@gmail.com>
2019-07-14 19:51:13 +0200

    Added initial route and default not found page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import NotFound from "./components/pages/NotFound";
+
 import "./App.css";
 
-const App = () => {
-  return <div className="App">My App</div>;
-};
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faGithub);
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  </Router>
+);
 
 export default App;
