82090985d16f922ec6b2d1594904317dcd40d079
Flash <flash.jaq@gmail.com>
2019-07-14 19:51:30 +0200

    Added navbar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+
+const Navbar = ({ title, icon }) => (
+  <nav className="navbar bg-primary">
+    <h1>
+      <FontAwesomeIcon icon={icon} /> {title}
+    </h1>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+Navbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: ["fab", "github"]
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.array.isRequired
+};
+
+export default Navbar;
