8d138f4723239d2c9f1ad040e4ecbf6433a7aedc
Flash <flash.jaq@gmail.com>
2019-07-15 17:57:53 +0200

    Added envolope

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,9 @@ import "./App.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faGithub);
+library.add(faGithub, faEnvelopeOpen);
 
 const App = () => (
   <ContactState>
