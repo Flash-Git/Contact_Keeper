c5df0796951198a51767a9827cbb17b4b1a14fa1
Flash <flash.jaq@gmail.com>
2019-07-16 03:23:25 +0200

    Added alerts

--- client/src/App.js
+++ client/src/App.js
@@ -3,38 +3,47 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelopeOpen, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelopeOpen,
+  faPhone,
+  faInfoCircle
+} from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alerts from "./components/layout/Alerts";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 import "./App.css";
 
-library.add(faGithub, faEnvelopeOpen, faPhone);
+library.add(faGithub, faEnvelopeOpen, faPhone, faInfoCircle);
 
 const App = () => (
   <AuthState>
     <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
+      <AlertState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Alerts />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </AlertState>
     </ContactState>
   </AuthState>
 );
