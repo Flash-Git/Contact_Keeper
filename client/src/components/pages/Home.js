b5254c09f27761873f5eaf3f390b780a4f4f606e
Flash <flash.jaq@gmail.com>
2019-07-16 16:05:12 +0200

    Added user loading on page load

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 
 import Contacts from "../contacts/Contacts";
 import ContactForm from "../contacts/ContactForm";
 import ContactFilter from "../contacts/ContactFilter";
+import AuthContext from "../../context/auth/AuthContext";
 
-const Home = () => (
-  <div className="grid-2">
-    <div>
-      <ContactForm />
+const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    //eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="grid-2">
+      <div>
+        <ContactForm />
+      </div>
+      <div>
+        <ContactFilter />
+        <Contacts />
+      </div>
     </div>
-    <div>
-      <ContactFilter />
-      <Contacts />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;
