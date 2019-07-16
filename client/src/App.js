277ae152052c062aad33b03c0415f7be10821f03
Flash <flash.jaq@gmail.com>
2019-07-16 17:32:40 +0200

    Added signout and and file contract icon

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import {
   faEnvelopeOpen,
   faPhone,
-  faInfoCircle
+  faInfoCircle,
+  faSignOutAlt,
+  faFileContract
 } from "@fortawesome/free-solid-svg-icons";
 
 import Navbar from "./components/layout/Navbar";
@@ -16,6 +18,7 @@ import Alerts from "./components/layout/Alerts";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
@@ -23,7 +26,14 @@ import setAuthToken from "./utils/setAuthToken";
 
 import "./App.css";
 
-library.add(faGithub, faEnvelopeOpen, faPhone, faInfoCircle);
+library.add(
+  faGithub,
+  faEnvelopeOpen,
+  faPhone,
+  faInfoCircle,
+  faSignOutAlt,
+  faFileContract
+);
 
 localStorage.token && setAuthToken(localStorage.token);
 
@@ -37,7 +47,7 @@ const App = () => (
             <div className="container">
               <Alerts />
               <Switch>
-                <Route exact path="/" component={Home} />
+                <PrivateRoute exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
