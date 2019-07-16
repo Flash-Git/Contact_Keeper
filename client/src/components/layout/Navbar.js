b9d0c45c23a4806cd02a1b36fad12f81f1822888
Flash <flash.jaq@gmail.com>
2019-07-16 16:57:07 +0200

    Added conditional navbar buttons

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 
-const Navbar = ({ title, icon }) => (
-  <nav className="navbar bg-primary">
-    <h1>
-      <FontAwesomeIcon icon={icon} /> {title}
-    </h1>
-    <ul>
+import AuthContext from "../../context/auth/AuthContext";
+
+const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, user, logout } = authContext;
+
+  const authLinks = (
+    <Fragment>
+      <li>
+        Hello <strong>{user && user.name}&nbsp;&nbsp;</strong>
+      </li>
       <li>
         <Link to="/">Home</Link>
       </li>
@@ -16,14 +22,41 @@ const Navbar = ({ title, icon }) => (
         <Link to="/about">About</Link>
       </li>
       <li>
-        <Link to="/register">Register</Link>
+        <a href="#!">
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
       </li>
       <li>
         <Link to="/login">Login</Link>
       </li>
-    </ul>
-  </nav>
-);
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+    </Fragment>
+  );
+
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <Link to="/">
+          <FontAwesomeIcon icon={icon} /> {title}
+        </Link>
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </nav>
+  );
+};
 
 Navbar.defaultProps = {
   title: "Contact Keeper",
